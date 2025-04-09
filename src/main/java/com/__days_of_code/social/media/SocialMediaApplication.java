1440aece82f42cb1350bce3adf745a7f0f49caf4
akins-b <akinsboms@gmail.com>
2025-04-09 08:59:16 +0100

    initial commit

---
+++ src/main/java/com/__days_of_code/social/media/SocialMediaApplication.java
@@ -0,0 +1,14 @@
+package com.__days_of_code.social.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/__days_of_code/social/media/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__days_of_code.social.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
